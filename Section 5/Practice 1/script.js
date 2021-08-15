2e6da9d452c83ce7a8587ad35098d4e0737f9a24
Derek Yong <dy0291@gmail.com>
2021-08-14 23:31:14 -0400

    Section 5-1 to Practice Finished

---
+++ Section 5/Practice 1/script.js
@@ -0,0 +1,7 @@
+/**
+ * Note: This file is intentionally empty.
+ * You can use it to test your skills at traversing the DOM using JavaScript.
+ */
+
+
+//  const main = document.querySelector(".maincontent .backpack");
